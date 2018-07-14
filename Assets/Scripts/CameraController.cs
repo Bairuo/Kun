af56444ad749c298cba28ab40962e9e0f2e28f62
Zhengru Jiang <jzr98@qq.com>
2018-07-14 22:09:16 +0700

    feat:Camera following under all azimuth and boundary, energe animations in different directions and some test by designer.It is noted that the number is temporarily not adjusted.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,22 +3,64 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
+    public GeminiController gemini;     // 边界
+    public Transform target;    // 摄像机跟随目标
+    public float offsetX = 4;    // 由于玩家宽度导致的边界偏移（扩大）
+    public float offsetY = 4;    // 由于玩家高度导致的边界偏移（扩大）
+    Vector2 buffer;
+    float originX;
     float originY;
     float originZ;
 
+    float leftBorder;
+    float rightBorder;
+    float topBorder;
+    float downBorder;
+
+    float width;        // 分辨率宽度对应的游戏世界宽度
+    float height;
+
 	// Use this for initialization
 	void Start () {
+        originX = transform.position.x;
         originY = transform.position.y;
         originZ = transform.position.z;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+        Vector3 cornerPos = Camera.main.ViewportToWorldPoint(new Vector3(1f, 1f, Mathf.Abs(Camera.main.transform.position.z)));
+
+        leftBorder = Camera.main.transform.position.x - (cornerPos.x - Camera.main.transform.position.x);
+        rightBorder = cornerPos.x;
+        topBorder = cornerPos.y;
+        downBorder = Camera.main.transform.position.y - (cornerPos.y - Camera.main.transform.position.y);
+
+        width = rightBorder - leftBorder;
+        height = topBorder - downBorder;
 	}
 
     void LateUpdate()
     {
-        transform.position = new Vector3(transform.position.x, originY, originZ);
+        
+        if(gemini.boundary[2] - offsetX + width / 2 < target.position.x && target.position.x < gemini.boundary[3] + offsetX - width / 2)
+        {
+            transform.position = new Vector3(target.position.x, originY, originZ);
+        }
+        else
+        {
+            transform.position = new Vector3(originX, originY, originZ);
+        }
+
+
+        if(gemini.boundary[0] + offsetY - height / 2 > target.position.y && target.position.y > gemini.boundary[1] - offsetY + height / 2)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y, originZ);
+        }
+        else
+        {
+            transform.position = new Vector3(transform.position.x, originY, originZ);
+        }
+
+        originX = transform.position.x;
+        originY = transform.position.y;
+        originZ = transform.position.z;
     }
 }
--- Assets/Scripts/EnergeController.cs
+++ Assets/Scripts/EnergeController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class EnergeController : MonoBehaviour {
+    public GameObject kun;
     public int energe = 100;
     public int minEnerge = 35;
     public float accelerateWaitTime = 2;
+    enum MoveState { left = -1, right = 1 };
+    int moveState = 1;
+    int lastMoveState = 1;
     int maxEnerge;
     int lastEnerge;
     float accelerateTimer = 0;
@@ -24,18 +28,30 @@ public class EnergeController : MonoBehaviour {
             accelerateTimer -= Time.deltaTime;
         }
 
+
+        // 透明度
         if(lastEnerge != energe)
         {
             lastEnerge = energe;
             Color color = GetComponent<SpriteRenderer>().color;
-            //GetComponent<SpriteRenderer>().color = new Color(color.r, color.g, color.b, 1.0f * energe / maxEnerge);
-            //GetComponentInChildren<SpriteRenderer>().color = new Color(color.r, color.g, color.b, 1.0f * energe / maxEnerge);
 
             SpriteRenderer[] sprites = GetComponentsInChildren<SpriteRenderer>();
             foreach(SpriteRenderer sprite in sprites)
             {
                 sprite.color = new Color(color.r, color.g, color.b, 1.0f * energe / maxEnerge);
             }
+        }
+
+        // 方向
+        if(kun.transform.position.x - this.transform.position.x != 0)
+        {
+            moveState = kun.transform.position.x - this.transform.position.x > 0 ? (int)MoveState.right : (int)MoveState.left;
+        }
+
+        if(moveState != lastMoveState)
+        {
+            GetComponent<Animator>().SetInteger("movestate", moveState);
+            lastMoveState = moveState;
         }
 	}
 
