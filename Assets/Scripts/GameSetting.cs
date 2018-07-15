89c88a4d202dbdee2a9f66ce815df4a18c5a7c6e
Zhengru Jiang <jzr98@qq.com>
2018-07-15 14:58:32 +0700

    feat:audio setting

---
+++ Assets/Scripts/GameSetting.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSetting : MonoBehaviour {
+
+	public void Exit()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
--- Assets/Scripts/StartScene.cs
+++ Assets/Scripts/StartScene.cs
@@ -4,9 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class StartScene : MonoBehaviour {
+    public GameObject setting;
 
 	public void StartGame()
     {
         SceneManager.LoadScene("冥界忘川河");
+    }
+
+    public void PopSetting()
+    {
+        if(!setting.activeSelf)
+        {
+            setting.SetActive(true);
+        }
+        else
+        {
+            setting.SetActive(false);
+        }
     }
 }
