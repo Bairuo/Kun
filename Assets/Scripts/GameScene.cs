a2c5d06edddbede4ec2d313e5fe1fdabf71ae08e
Zhengru Jiang <jzr98@qq.com>
2018-07-15 10:40:33 +0700

    feat:return, pause and restart of game scene

---
+++ Assets/Scripts/GameScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameScene : MonoBehaviour {
+
+	public void Return()
+    {
+        SceneManager.LoadScene("开始游戏");
+    }
+
+    public void Pause()
+    {
+        if(Time.timeScale == 0)
+        {
+            Time.timeScale = 1;
+        }
+        else
+        {
+            Time.timeScale = 0;
+        }
+    }
+
+    public void ReStart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
