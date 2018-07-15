d1c8a33e05ba5f7d4fad61cc6516eca2565705ed
Zhengru Jiang <jzr98@qq.com>
2018-07-15 15:30:39 +0700

    feat:curtain before game start

---
+++ Assets/Curtain.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Curtain : MonoBehaviour {
+
+	public void StartGame()
+    {
+        SceneManager.LoadScene("开始游戏");
+    }
+}
