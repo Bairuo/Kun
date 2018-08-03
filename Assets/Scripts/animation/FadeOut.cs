e8afdc6c7cbfac3b2fc3e96dd9d3c19431e58584
Zhengru Jiang <jzr98@qq.com>
2018-08-03 12:43:45 +0700

    the rocker can be activated in the dark

--- Assets/Scripts/animation/FadeOut.cs
+++ Assets/Scripts/animation/FadeOut.cs
@@ -6,25 +6,93 @@ using UnityEngine.UI;
 public class FadeOut : MonoBehaviour {
     public float startTime = 0;
     public float FadeTime;
+    public float TargetAlpha = 1;
+    public bool AutoTarget = false;
+    public bool CanBeInterrupted = true;
     float timer = 0;
+
+    void Start()
+    {
+        Color color;
+        if(GetComponent<SpriteRenderer>() != null)
+        {
+            color = GetComponent<SpriteRenderer>().color;
+            GetComponent<SpriteRenderer>().color = new Color(color.r, color.g, color.b, 0);
+        }
+        else if(GetComponent<Image>() != null)
+        {
+            color = GetComponent<Image>().color;
+            GetComponent<Image>().color = new Color(color.r, color.g, color.b, 0);
+        }
+        else
+        {
+            return;
+        }
+
+        if(AutoTarget)
+        {
+            TargetAlpha = color.a;
+        }
+    }
 
     void FixedUpdate()
     {
+        Color color;
         timer += Time.deltaTime;
 
         if(timer > startTime)
         {
             float t = timer - startTime;
-            Color color = GetComponent<SpriteRenderer>().color;
+            //Color color = GetComponent<SpriteRenderer>().color;
             
             if(t > FadeTime)
             {
-                GetComponent<SpriteRenderer>().color = new Color(color.r, color.g, color.b, 1.0f);
+                if (GetComponent<SpriteRenderer>() != null)
+                {
+                    color = GetComponent<SpriteRenderer>().color;
+                    GetComponent<SpriteRenderer>().color = new Color(color.r, color.g, color.b, TargetAlpha);
+                }
+                else if (GetComponent<Image>() != null)
+                {
+                    color = GetComponent<Image>().color;
+                    GetComponent<Image>().color = new Color(color.r, color.g, color.b, TargetAlpha);
+                }
+
                 this.enabled = false;
             }
             else
             {
-                GetComponent<SpriteRenderer>().color = new Color(color.r, color.g, color.b, t / FadeTime);
+                float alpha = t / FadeTime * TargetAlpha;
+                if (GetComponent<SpriteRenderer>() != null)
+                {
+                    color = GetComponent<SpriteRenderer>().color;
+
+                    // 外部打断
+                    if (CanBeInterrupted && color.a > alpha)
+                    {
+                        this.enabled = false;
+                        return;
+                    }
+
+                    GetComponent<SpriteRenderer>().color = new Color(color.r, color.g, color.b, alpha);
+                }
+                else if (GetComponent<Image>() != null)
+                {
+                    color = GetComponent<Image>().color;
+
+                    // 外部打断
+                    if (CanBeInterrupted && color.a > alpha)
+                    {
+                        this.enabled = false;
+                        return;
+                    }
+
+                    GetComponent<Image>().color = new Color(color.r, color.g, color.b, alpha);
+                }
+                else
+                {
+                    return;
+                }
             }
             
         }
