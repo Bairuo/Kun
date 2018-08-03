5fb1b56aac3ca399d8629259eb95ac9f833a2494
Zhengru Jiang <jzr98@qq.com>
2018-08-03 23:45:36 +0700

    new start scene

--- Assets/Scripts/physics/GravitationalField.cs
+++ Assets/Scripts/physics/GravitationalField.cs
@@ -2,18 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// 吸引力，不是现实中的引力模型
 public class GravitationalField : MonoBehaviour {
     public string target = "Kun";
-    public float radius;            // 无吸引力范围
-    public float gravitation;       // 吸引力大小
+    public float safeRadius;            // 无引力范围
+    public float constant;       // 引力系数
 
     void OnTriggerStay2D(Collider2D other)
     {
         Vector2 forward = transform.position - other.transform.position;
-        if(other.tag == target && forward.magnitude > radius)
+        if(other.tag == target && forward.magnitude > safeRadius)
         {
-            other.GetComponent<Rigidbody2D>().AddForce(forward.normalized * gravitation, ForceMode2D.Impulse);
+            float dis = forward.magnitude;
+            other.GetComponent<Rigidbody2D>().AddForce(constant / Mathf.Pow(dis, 2) * forward.normalized, ForceMode2D.Impulse);
         }
     }
 }
---
+++ Assets/Scripts/scene_start/Leaf.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Leaf : MonoBehaviour {
+    public float strength = 100;
+    public float rotate = 2;
+    public float maxSpeed = 5;
+
+	void Start () {
+        float emissionAngle = Random.Range(0, 2 * Mathf.PI);
+        GetComponent<Rigidbody2D>().AddForce(new Vector2(Mathf.Sin(emissionAngle), Mathf.Cos(emissionAngle)) * strength);
+        GetComponent<Rigidbody2D>().AddTorque(Random.Range(0, 2 * Mathf.PI) * rotate);
+	}
+	
+    void FixedUpdate()
+    {
+        //if(GetComponent<Rigidbody2D>().velocity.magnitude > maxSpeed)
+        //{
+        //    GetComponent<Rigidbody2D>().velocity *= maxSpeed / GetComponent<Rigidbody2D>().velocity.magnitude;
+        //}
+    }
+}
