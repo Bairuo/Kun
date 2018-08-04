bd1d049a33b177fa7a48fff51aa59c70a0161452
Zhengru Jiang <jzr98@qq.com>
2018-08-04 13:51:03 +0700

    dictionary modify, close develop model and bgm of level2 add

---
+++ Assets/Scripts/audio/AudioSetting.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSetting : MonoBehaviour {
+    static float audioVolume = 0.618f;
+    static float audioEffectVolume = 0.618f;
+
+    public Slider AudioSlider;    // 音乐
+    public Slider AudioEffectSlider;      // 音效   
+
+    public AudioSource audio;
+    public AudioSource[] audioEffects;
+
+    void Start()
+    {
+        AudioSlider.value = audioVolume;
+        AudioEffectSlider.value = audioEffectVolume;
+
+        ChangeAudio();
+        ChangeAudioEffects();
+    }
+
+	public void ChangeAudio()
+    {
+        audio.volume = AudioSlider.value;
+        audioVolume = AudioSlider.value;
+    }
+
+    public void ChangeAudioEffects()
+    {
+        foreach(var audio in audioEffects)
+        {
+            audio.volume = AudioEffectSlider.value;
+        }
+        audioEffectVolume = AudioEffectSlider.value;
+    }
+}
