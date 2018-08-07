85049271c1b888558973c048cde1cf141cb2f576
Zhengru Jiang <jzr98@qq.com>
2018-08-07 14:22:44 +0700

    feat: collapse of level2

--- Assets/Scripts/animation/Mover.cs
+++ Assets/Scripts/animation/Mover.cs
@@ -3,14 +3,52 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Mover : MonoBehaviour {
+    public AnimationCurve curveX;
+    public AnimationCurve curveY;
+    public float startTime;
+    public float constant = 1;
+    public bool freezeX = true;
+    public bool freezeY = false;
+    float originalX;
+    float originalY;
+    float timer;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    void Start()
+    {
+        originalX = transform.position.x;
+        originalY = transform.position.y;
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+        timer += Time.deltaTime;
+
+        if(timer >= startTime)
+        {
+            float shiftX = curveX.Evaluate(timer - startTime) * constant;
+            float shiftY = curveY.Evaluate(timer - startTime) * constant;
+            float x;
+            float y;
+
+            if(freezeX)
+            {
+                x = originalX;
+            }
+            else
+            {
+                x = originalX + shiftX;
+            }
+
+            if(freezeY)
+            {
+                y = originalY;
+            }
+            else
+            {
+                y = originalY + shiftY;
+            }
+
+            transform.position = new Vector3(x, y, 0);
+        }
 	}
 }
