824cdf32273f0dd6447da52263f70d74801ef701
Zhengru Jiang <jzr98@qq.com>
2018-08-09 11:01:12 +0700

    feat: grass of level3

---
+++ Assets/Scripts/event/OnTriggerEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnTriggerEvent : MonoBehaviour {
+    public string target;
+    [SerializeField]
+    public UnityEvent onTriggerEvent = new UnityEvent();
+    bool hasActived = false;
+
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == target && !hasActived)
+        {
+            onTriggerEvent.Invoke();
+            hasActived = true;
+        }
+    }
+}
