047baa5b2ba8d1b7500fc8d7f6640daea0cf1d78
Jiamu Sun <x739566858@outlook.com>
2018-08-09 16:17:26 +0800

    feat:add energybar.

---
+++ Assets/Scripts/ui_game/EnergyBarController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnergyBarController : MonoBehaviour {
+    public GameObject kun;
+    public GameObject energy;
+    public Image divider;
+    KunController kc;
+    EnergeController ec;
+    Slider slider;
+    public int totalvalue; // 鲲和伴随物的总能量，对应着滑条的值
+    public int kunvalue; // 鲲的能量
+    public int geminivalue;  //伴随物能量
+    float targetvalue;
+    public float targetdividerposition;
+    // Use this for initialization
+    void Start() {
+        kc = kun.GetComponent<KunController>();
+        ec = energy.GetComponent<EnergeController>();
+        slider = GetComponent<Slider>();
+        slider.value = 1;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        kunvalue = kc.energe;
+        geminivalue = ec.energe;
+        totalvalue = kunvalue + geminivalue;
+        targetvalue = (float)totalvalue / 200;
+        if (slider.value != targetvalue)
+        {
+            if (slider.value < targetvalue)
+                slider.value += 0.001f;
+            else
+                slider.value -= 0.001f;
+        }
+        targetdividerposition = targetvalue * ((float)kunvalue / totalvalue);
+        if(getdividerposition() != targetdividerposition)
+        {
+            if (getdividerposition() < targetdividerposition)
+                dividerlocate(getdividerposition() + 0.001f);
+            else
+                dividerlocate(getdividerposition() - 0.001f);
+        }
+    }
+
+    void dividerlocate(float position)
+    {
+        float posx = 696 * position;
+        posx -= 348;
+        divider.rectTransform.anchoredPosition = new Vector3(posx, -5.9f, 0f);
+    }
+    float getdividerposition()
+    {
+        float posx = divider.rectTransform.anchoredPosition.x;
+        return (posx + 348) / 696;
+    }
+}
