5409648355568e4f6f35527ebb7a59630ade26d3
Zhengru Jiang <jzr98@qq.com>
2018-08-10 15:21:44 +0700

    level6

---
+++ Assets/Scripts/scene_game/level6/MaskController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaskController : MonoBehaviour {
+    public float target;
+    public float endY;
+    public float startY;
+    public bool autoStart = false;
+
+	// Use this for initialization
+	void Start () {
+        if(autoStart)
+        {
+            startY = transform.position.y;
+        }
+	}
+	
+	void FixedUpdate () {
+        float scale = Mathf.Lerp(1, target, (transform.position.y - startY) / (endY - startY));
+        transform.localScale = new Vector3(scale, scale, 1);
+	}
+}
